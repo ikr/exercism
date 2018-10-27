7499eb534b85a2c59d9e02ef87a4982bd8b4ac3a
Ivan Krechetov <ikr@ikr.su>
2018-10-27 10:42:39 +0200

    Save a sequence scan

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -3,9 +3,10 @@ import kotlin.math.*
 object Hamming {
     fun compute(s1: String, s2: String): Int =
         if (s1.length == s2.length)
-            s1.zip(s2)
-                .map({it.first.compareTo(it.second).absoluteValue})
-                .sum()
+            s1.zip(s2).fold(
+                0,
+                {memo, pair -> memo + pair.first.compareTo(pair.second).absoluteValue}
+            )
         else
             throw IllegalArgumentException(
                 "left and right strands must be of equal length."
