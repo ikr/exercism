82d11e3c38ea51a4ae02c24111ea7c627584e2d3
Ivan Krechetov <ikr@ikr.su>
2018-11-07 20:51:58 +0100

    Refactor

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -4,19 +4,23 @@ object Luhn {
 
         if (noSpaces.length < 2 || !(Regex("^[0-9]+$") matches noSpaces)) return false
 
-        val digits = noSpaces.map {it.toString().toInt()}.reversed()
+        val allDoubledCapped = noSpaces
+            .reversed()
+            .map {it.toString().toInt()}
+            .mapIndexed {i, d ->
+                if (i % 2 == 1)
+                    doubleCapped(d)
+                else
+                    d
+            }
 
-        val capDoubled = digits.mapIndexed {i, d ->
-            if (i % 2 == 1)
-                capDouble(d)
-            else
-                d
-        }
-
-        return capDoubled.sum() % 10 == 0
+       return allDoubledCapped.sum() % 10 == 0
     }
 }
 
-private fun capDouble(x: Int) =
-    if (x * 2 > 9) x * 2 - 9
-    else x * 2
+private fun doubleCapped(x: Int): Int {
+    val x_2 = x * 2
+
+    return if (x_2 > 9) x_2 - 9
+    else x_2
+}
