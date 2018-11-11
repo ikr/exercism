28f4784459432b5ed38094259f443b2f2f447617
Ivan Krechetov <ikr@ikr.su>
2018-11-11 11:57:57 +0100

    Solve the ISBN Verifier

---
+++ kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
@@ -0,0 +1,22 @@
+class IsbnVerifier {
+    fun isValid(s: String): Boolean {
+        val digitsString = s.filter(::isValidDigit)
+
+        if (!(Regex("^[0-9]{9}[0-9X]$") matches digitsString)) return false
+
+        val ds = digitsString.map(::digit)
+        return ds.zip(10 downTo 1).toList().map(::mul).sum() % 11 == 0
+    }
+}
+
+private fun digit(c: Char): Int =
+    if (c == 'X')
+        10
+    else
+        c.toString().toInt()
+
+private fun isValidDigit(c: Char) =
+    c == 'X' || (c >= '0' && c <= '9')
+
+private fun mul(pair: Pair<Int, Int>): Int =
+    pair.first * pair.second
