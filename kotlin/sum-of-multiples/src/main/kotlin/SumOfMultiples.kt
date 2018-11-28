d56cd5518942b584bb84cb55ac8316cd60d5fb80
Ivan Krechetov <ikr@ikr.su>
2018-11-28 21:24:55 +0100

    Solve the Sum of Multiples

--- kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,4 +1,10 @@
+import kotlin.sequences.generateSequence
+
 object SumOfMultiples {
     fun sum(xs: Set<Int>, limit: Int): Int =
-        0
+        xs.flatMap { multiples(it, limit).toList() }.toSet().sum()
 }
+
+fun multiples(x: Int, limit: Int): Sequence<Int> =
+    generateSequence({ if (x < limit) x else null })
+        { if (it + x < limit) it + x else null }
