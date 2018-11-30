2d7026d38e1eac7548ff4e6a69bf10604333b379
Ivan Krechetov <ikr@ikr.su>
2018-11-30 20:59:47 +0100

    Solve the Secret Handshake

---
+++ kotlin/secret-handshake/src/main/kotlin/Signal.kt
@@ -0,0 +1,26 @@
+object HandshakeCalculator {
+    fun calculateHandshake(x: Int): List<Signal> {
+        val binWithPrefix = x.toString(2).padStart(5, '0')
+        val reverse = binWithPrefix[0] == '1'
+        val bin = binWithPrefix.substring(1)
+
+        val result = bin
+            .toList()
+            .zip(Signal.values().reversed())
+            .fold(
+                mutableListOf<Signal>(),
+                {agg, p ->
+                    val (digit, signal) = p
+                    if (digit == '1') agg.add(0, signal)
+                    agg
+                }
+            )
+
+        return if (reverse)
+            result.reversed()
+        else
+            result
+    }
+}
+
+enum class Signal { WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP }
