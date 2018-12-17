cf6d001d36c1c20d16abad6d4b42876d0851578c
Ivan Krechetov <ikr@ikr.su>
2018-12-17 19:22:50 +0100

    Compute doReverse binary

--- kotlin/secret-handshake/src/main/kotlin/Signal.kt
+++ kotlin/secret-handshake/src/main/kotlin/Signal.kt
@@ -1,7 +1,7 @@
 object HandshakeCalculator {
     fun calculateHandshake(x: Int): List<Signal> {
         val binWithPrefix = x.toString(2).padStart(5, '0')
-        val doReverse = binWithPrefix[0] == '1'
+        val doReverse = (x and (1 shl 4)) != 0
         val bin = binWithPrefix.substring(1)
 
         val result = bin
