01ed5d042b5641a401cca7f7fdd1c3776627bb88
Ivan Krechetov <ikr@ikr.su>
2018-12-18 10:46:23 +0100

    Go smarter about it

--- kotlin/grains/src/main/kotlin/Board.kt
+++ kotlin/grains/src/main/kotlin/Board.kt
@@ -2,12 +2,10 @@ import java.math.BigInteger
 
 object Board {
     fun getGrainCountForSquare(x: Int): BigInteger {
-        require(x > 0 && x < 65) { "Only integers between 1 and 64 (inclusive) are allowed" }
+        require(x in 1..64) { "Only integers between 1 and 64 (inclusive) are allowed" }
         return BigInteger.valueOf(2).pow(x - 1)
     }
 
     fun getTotalGrainCount(): BigInteger =
-        (1..64)
-            .map(Board::getGrainCountForSquare)
-            .reduce(BigInteger::add)
+        BigInteger.valueOf(2).pow(64) - BigInteger.ONE
 }
