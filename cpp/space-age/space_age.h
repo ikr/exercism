ca0b7febb565d25279080a28a46e8e9951b2bca5
Ivan Krechetov <ikr@ikr.su>
2019-03-15 16:08:37 +0100

    Follow the mentor's advice

--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -3,20 +3,56 @@
 
 namespace space_age {
 struct space_age {
-    space_age(const double age_in_seconds_);
-    double seconds() const;
+    constexpr space_age(const double age_in_seconds_)
+        : age_in_seconds(age_in_seconds_){};
 
-    double on_earth() const;
-    double on_jupiter() const;
-    double on_mars() const;
-    double on_mercury() const;
-    double on_neptune() const;
-    double on_saturn() const;
-    double on_uranus() const;
-    double on_venus() const;
+    constexpr double seconds() const { return age_in_seconds; }
+
+    constexpr double on_earth() const {
+        return age_in_seconds / earth_year_seconds;
+    }
+
+    constexpr double on_jupiter() const {
+        return on_earth() / orbital_period_in_earth_years::jupiter;
+    }
+
+    constexpr double on_mars() const {
+        return on_earth() / orbital_period_in_earth_years::mars;
+    }
+
+    constexpr double on_mercury() const {
+        return on_earth() / orbital_period_in_earth_years::mercury;
+    }
+
+    constexpr double on_neptune() const {
+        return on_earth() / orbital_period_in_earth_years::neptune;
+    }
+
+    constexpr double on_saturn() const {
+        return on_earth() / orbital_period_in_earth_years::saturn;
+    }
+
+    constexpr double on_uranus() const {
+        return on_earth() / orbital_period_in_earth_years::uranus;
+    }
+
+    constexpr double on_venus() const {
+        return on_earth() / orbital_period_in_earth_years::venus;
+    }
 
   private:
     const double age_in_seconds;
+    static constexpr double earth_year_seconds = 31557600;
+
+    struct orbital_period_in_earth_years {
+        static constexpr double jupiter = 11.862615;
+        static constexpr double mars = 1.8808158;
+        static constexpr double mercury = 0.2408467;
+        static constexpr double neptune = 164.79132;
+        static constexpr double saturn = 29.447498;
+        static constexpr double uranus = 84.016846;
+        static constexpr double venus = 0.61519726;
+    };
 };
 } // namespace space_age
 
