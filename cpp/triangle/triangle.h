b6726db2eeec258b324f720d1a53af1cf18d0c0c
Ivan Krechetov <ikr@ikr.su>
2019-03-16 08:21:09 +0100

    Make it runnable

--- cpp/triangle/triangle.h
+++ cpp/triangle/triangle.h
@@ -6,7 +6,10 @@
 namespace triangle {
 enum class flavor { equilateral, isosceles, scalene };
 
-constexpr flavor kind(const double a, const double b, const double c);
+constexpr flavor kind(const double a, const double b, const double c) {
+    return a == b && b == c ? flavor::equilateral : flavor::scalene;
+}
+
 } // namespace triangle
 
 #endif // TRIANGLE_H
