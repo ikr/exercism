29cbbc3c2cfe30bf50bd78e62ee425314736e161
Ivan Krechetov <ikr@ikr.su>
2019-03-19 19:43:13 +0100

    Simplify

--- cpp/grains/grains.cpp
+++ cpp/grains/grains.cpp
@@ -3,19 +3,7 @@
 #include <cmath>
 
 long fast_exponent_of_2(const int power) {
-    static long cache[grains::MAX_SQUARE_NUM]{1};
-    assert(power >= 0 && power < grains::MAX_SQUARE_NUM);
-
-    if (!cache[power]) {
-        const long value_on_half = fast_exponent_of_2(power / 2);
-
-        if (power % 2)
-            cache[power] = 2 * value_on_half * value_on_half;
-        else
-            cache[power] = value_on_half * value_on_half;
-    }
-
-    return cache[power];
+    return 1L << power;
 }
 
 long grains::square(const int num) {
