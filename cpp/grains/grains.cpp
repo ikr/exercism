f47f9fcec8faeacd108fa9716e7b02f5304a03ef
Ivan Krechetov <ikr@ikr.su>
2019-03-19 19:21:54 +0100

    Implement an own fast_exponent_of_2

--- cpp/grains/grains.cpp
+++ cpp/grains/grains.cpp
@@ -1,15 +1,25 @@
 #include "grains.h"
 #include <cassert>
-#include <cmath>
+
+long fast_exponent_of_2(const int power) {
+    static long cache[grains::MAX_SQUARE_NUM]{1};
+
+    if (!cache[power]) {
+        const long value_on_half = fast_exponent_of_2(power / 2);
+
+        if (power % 2) {
+            cache[power] = 2 * value_on_half * value_on_half;
+        } else {
+            cache[power] = value_on_half * value_on_half;
+        }
+    }
+
+    return cache[power];
+}
 
 long grains::square(const int num) {
-    static long cache[MAX_SQUARE_NUM + 1]{};
     assert(num >= 1 && num <= MAX_SQUARE_NUM);
-
-    if (!cache[num])
-        cache[num] = std::pow(2, num - 1);
-
-    return cache[num];
+    return fast_exponent_of_2(num - 1);
 }
 
 long grains::total() {
