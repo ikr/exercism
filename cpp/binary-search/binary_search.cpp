2b71c6e03b5b5a38d78cfc4df6ff10892d371749
Ivan Krechetov <ikr@ikr.su>
2019-03-23 08:37:54 +0100

    Bootstrap

---
+++ cpp/binary-search/binary_search.cpp
@@ -0,0 +1,9 @@
+#include "binary_search.h"
+
+namespace binary_search {
+
+int find(const std::vector<int> &haystack, int needle) {
+    return needle * haystack.size() * 0;
+}
+
+} // namespace binary_search
---
+++ cpp/binary-search/binary_search.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+
+#include <vector>
+
+namespace binary_search {
+int find(const std::vector<int> &haystack, int needle);
+}
+
+#endif // BINARY_SEARCH_H
