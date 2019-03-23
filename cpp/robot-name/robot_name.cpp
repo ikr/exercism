8ab9af083f0d2b52b71acdc243cb5e294eaf29a1
Ivan Krechetov <ikr@ikr.su>
2019-03-23 10:52:20 +0100

    Implement the names generation

--- cpp/robot-name/robot_name.cpp
+++ cpp/robot-name/robot_name.cpp
@@ -2,15 +2,45 @@
 
 namespace robot_name {
 
-robot::robot() : id{} {}
+robot::robot() : id{new_name()} {}
 
-void robot::reset() {}
+void robot::reset() { id = new_name(); }
 
-std::random_device &robot::provide_random_device() {
-    static std::random_device r_dev;
-    return r_dev;
+std::string robot::new_name() {
+    return random_sequence(provide_letters_distribution(), name_letters_count) +
+           random_sequence(provide_digits_distribution(), name_digits_count);
 }
 
-std::mt19937 robot::engine{provide_random_device()()};
+std::string
+robot::random_sequence(std::uniform_int_distribution<short> &distribution,
+                       const size_t length) {
+    std::string result(length, ' ');
+
+    for (size_t i = 0; i != length; ++i) {
+        result[i] = static_cast<char>(distribution(provide_random_generator()));
+    }
+
+    return result;
+}
+
+std::random_device &robot::provide_random_device() {
+    static std::random_device result;
+    return result;
+}
+
+std::mt19937 &robot::provide_random_generator() {
+    static std::mt19937 result{provide_random_device()()};
+    return result;
+}
+
+std::uniform_int_distribution<short> &robot::provide_letters_distribution() {
+    static std::uniform_int_distribution<short> result{'A', 'Z'};
+    return result;
+}
+
+std::uniform_int_distribution<short> &robot::provide_digits_distribution() {
+    static std::uniform_int_distribution<short> result{'0', '9'};
+    return result;
+}
 
 } // namespace robot_name
--- cpp/robot-name/robot_name.h
+++ cpp/robot-name/robot_name.h
@@ -1,7 +1,7 @@
 #ifndef ROBOT_NAME_H
 #define ROBOT_NAME_H
-#include <string>
 #include <random>
+#include <string>
 
 namespace robot_name {
 struct robot {
@@ -12,9 +12,20 @@ struct robot {
   private:
     std::string id;
 
-  static std::random_device& provide_random_device();
-  static std::mt19937 engine;
+    static constexpr size_t name_letters_count = 2;
+    static constexpr size_t name_digits_count = 3;
+
+    static std::string new_name();
+
+    static std::string
+    random_sequence(std::uniform_int_distribution<short> &distribution,
+                    const size_t length);
+
+    static std::random_device &provide_random_device();
+    static std::mt19937 &provide_random_generator();
+    static std::uniform_int_distribution<short> &provide_letters_distribution();
+    static std::uniform_int_distribution<short> &provide_digits_distribution();
 };
 } // namespace robot_name
 
-#endif //ROBOT_NAME_H
+#endif // ROBOT_NAME_H
