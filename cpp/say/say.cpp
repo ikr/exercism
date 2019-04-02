ba215fad3cad5049f332c88bb74731c67f068576
Ivan Krechetov <ikr@ikr.su>
2019-04-02 09:53:50 +0200

    Bootstrap the “Say”

---
+++ cpp/say/say.cpp
@@ -0,0 +1,7 @@
+#include "say.h"
+
+namespace say {
+
+std::string in_english(unsigned long long x) { return std::to_string(x); }
+
+} // namespace say
---
+++ cpp/say/say.h
@@ -0,0 +1,10 @@
+#ifndef SAY_H
+#define SAY_H
+
+#include <string>
+
+namespace say {
+std::string in_english(unsigned long long x);
+}
+
+#endif // SAY_H
