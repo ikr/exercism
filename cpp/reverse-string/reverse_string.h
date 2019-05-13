7a9d2fba3377b3f07f4a4ff328ca1dfad10ea670
Ivan Krechetov <ikr@ikr.su>
2019-05-13 15:23:18 +0200

    Solve the reverse-string

---
+++ cpp/reverse-string/reverse_string.h
@@ -0,0 +1,9 @@
+#ifndef REVERSE_STRING_H
+#define REVERSE_STRING_H
+#include <string>
+
+namespace reverse_string {
+std::string reverse_string(const std::string &s);
+}
+
+#endif // REVERSE_STRING_H
