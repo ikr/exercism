346e8f61f61eac0d49fb5fe8523b5ba2530b7957
Ivan Krechetov <ikr@ikr.su>
2019-05-15 09:40:02 +0200

    Favor inline-ing

--- cpp/reverse-string/reverse_string.cpp
+++
@@ -1,9 +0,0 @@
-#include "reverse_string.h"
-
-namespace reverse_string {
-
-std::string reverse_string(const std::string &s) {
-    return std::string(s.rbegin(), s.rend());
-}
-
-} // namespace reverse_string
--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -3,7 +3,11 @@
 #include <string>
 
 namespace reverse_string {
-std::string reverse_string(const std::string &s);
+
+inline std::string reverse_string(const std::string &s) {
+    return std::string(s.rbegin(), s.rend());
 }
+
+} // namespace reverse_string
 
 #endif // REVERSE_STRING_H
