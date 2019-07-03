020aebe7f786fbfe8418da92e944054bfd6042b9
Ivan Krechetov <ikr@ikr.su>
2019-07-03 20:09:47 +0200

    Bootstrap the “Sieve”

---
+++ cpp/sieve/sieve.cpp
@@ -0,0 +1,5 @@
+#include "sieve.h"
+
+namespace sieve {
+std::vector<int> primes(const int limit) { return {limit}; }
+} // namespace sieve
---
+++ cpp/sieve/sieve.h
@@ -0,0 +1,10 @@
+#ifndef SIEVE_H
+#define SIEVE_H
+
+#include <vector>
+
+namespace sieve {
+std::vector<int> primes(const int limit);
+} // namespace sieve
+
+#endif // SIEVE_H
