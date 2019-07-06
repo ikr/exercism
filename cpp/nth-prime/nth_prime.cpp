3a8502efe130d3feaa2fdea388c682a1c55dc629
Ivan Krechetov <ikr@ikr.su>
2019-07-06 10:51:51 +0200

    Save some memory
    No need to keep the whole ans vector of primes up to the nth one.

--- cpp/nth-prime/nth_prime.cpp
+++ cpp/nth-prime/nth_prime.cpp
@@ -17,30 +17,29 @@ constexpr int nth_prime_upper_bound(const int n) {
 /**
  * See https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
  */
-std::vector<int> sieve_primes(const int limit) {
-    std::vector<int> ans;
-    std::vector<bool> scratchpad(limit + 1, true);
+int sieve_primes_return_nth(const int value_limit, const int n) {
+    std::vector<bool> scratchpad(value_limit + 1, true);
     std::fill_n(scratchpad.begin(), 2, false);
 
+    int count = 1;
     for (auto i = scratchpad.begin() + 2; i != scratchpad.end();
-         i = std::find(i + 1, scratchpad.end(), true)) {
+         i = std::find(i + 1, scratchpad.end(), true), ++count) {
         const int current_divider = std::distance(scratchpad.begin(), i);
-        ans.push_back(current_divider);
+        if (count == n) return current_divider;
 
-        for (auto idx = 2 * current_divider; idx <= limit;
+        for (auto idx = 2 * current_divider; idx <= value_limit;
              idx += current_divider) {
             scratchpad[idx] = false;
         }
     }
 
-    return ans;
+    throw std::logic_error("Couldn't reach the nth prime in the sieve");
 }
 } // namespace
 
 namespace prime {
 int nth(const int n) {
     if (n < 1) throw std::domain_error("Primes are counted from 1");
-    const auto primes = sieve_primes(nth_prime_upper_bound(n));
-    return primes[n - 1];
+    return sieve_primes_return_nth(nth_prime_upper_bound(n), n);
 }
 } // namespace prime
