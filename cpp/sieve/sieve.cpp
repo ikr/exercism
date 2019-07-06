bd0ab99dd8a05b6554481db58be09daa659ba52c
Ivan Krechetov <ikr@ikr.su>
2019-07-06 08:30:54 +0200

    Save the final loop, form the answer right away

--- cpp/sieve/sieve.cpp
+++ cpp/sieve/sieve.cpp
@@ -4,12 +4,14 @@
 
 namespace sieve {
 std::vector<int> primes(const int limit) {
+    std::vector<int> ans;
     std::vector<bool> scratchpad(limit + 1, true);
     std::fill_n(scratchpad.begin(), 2, false);
 
     for (auto i = scratchpad.begin() + 2; i != scratchpad.end();
          i = std::find(i + 1, scratchpad.end(), true)) {
         const int current_divider = std::distance(scratchpad.begin(), i);
+        ans.push_back(current_divider);
 
         for (auto idx = 2 * current_divider; idx <= limit;
              idx += current_divider) {
@@ -17,10 +19,6 @@ std::vector<int> primes(const int limit) {
         }
     }
 
-    std::vector<int> ans;
-    for (auto idx = 2; idx <= limit; ++idx) {
-        if (scratchpad[idx]) ans.push_back(idx);
-    }
     return ans;
 }
 } // namespace sieve
