aa1722e8f6ad53678f0c6ba888009569fb0920bb
Ivan Krechetov <ikr@ikr.su>
2019-07-07 15:05:37 +0200

    Solve “from the other side”

--- cpp/sum-of-multiples/sum_of_multiples.cpp
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -21,20 +21,6 @@ void dedupe(std::vector<int> &xs) {
     std::sort(xs.begin(), xs.end());
     xs.erase(std::unique(xs.begin(), xs.end()), xs.end());
 }
-
-std::vector<std::vector<int>> non_empty_subsets(const std::vector<int> &xs) {
-    std::vector<std::vector<int>> ans;
-
-    for (int bits = 1; bits != (1 << xs.size()); ++bits) {
-        std::vector<int> subset;
-        for (auto i = 0; i != static_cast<int>(xs.size()); ++i)
-            if (bits & (1 << i)) subset.push_back(xs[i]);
-
-        ans.push_back(subset);
-    }
-
-    return ans;
-}
 } // namespace
 
 namespace sum_of_multiples {
@@ -42,16 +28,13 @@ int to(std::vector<int> xs, const int upper_bound) {
     dedupe(xs);
     remove_multiples_of_others(xs);
 
-    std::unordered_set<int> products;
+    int ans = 0;
 
-    for (const auto &basis : non_empty_subsets(xs)) {
-        const int p = std::accumulate(basis.cbegin(), basis.cend(), 1,
-                                      std::multiplies<int>());
+    for (int i = 1; i < upper_bound; ++i)
+        if (any_of(xs.cbegin(), xs.cend(),
+                   [i](const int x) { return i % x == 0; }))
+            ans += i;
 
-        for (int factor = 1; factor * p < upper_bound; ++factor)
-            products.insert(factor * p);
-    }
-
-    return std::accumulate(products.cbegin(), products.cend(), 0);
+    return ans;
 }
 } // namespace sum_of_multiples
