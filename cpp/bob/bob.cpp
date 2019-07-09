44b0792c1b1b9da48ebc4daa83b413752cce7663
Ivan Krechetov <ikr@ikr.su>
2019-07-09 14:37:17 +0200

    Refactor to a sort of better structure

--- cpp/bob/bob.cpp
+++ cpp/bob/bob.cpp
@@ -8,18 +8,18 @@ const std::locale &locale() {
     return l;
 }
 
-bool is_silence(std::string phrase) {
-    return all_of(phrase.begin(), phrase.end(),
-                  [](const char c) { return std::isspace(c, locale()); });
+bool is_letter(const char c) { return std::isalpha(c, locale()); }
+bool is_space(const char c) { return std::isspace(c, locale()); }
+bool is_upper_or_nor_leter(const char c) {
+    return std::isupper(c, locale()) || !is_letter(c);
 }
 
-bool is_letter(const char c) { return std::isalpha(c, locale()); }
+bool is_silence(std::string phrase) {
+    return all_of(phrase.begin(), phrase.end(), is_space);
+}
 
 bool is_yelling(const std::string &phrase) {
-    return std::all_of(phrase.cbegin(), phrase.cend(),
-                       [](const char c) {
-                           return std::isupper(c, locale()) || !is_letter(c);
-                       }) &&
+    return std::all_of(phrase.cbegin(), phrase.cend(), is_upper_or_nor_leter) &&
            std::any_of(phrase.cbegin(), phrase.cend(), is_letter);
 }
 
@@ -33,9 +33,11 @@ namespace bob {
 std::string hey(const std::string &phrase) {
     if (is_silence(phrase)) return "Fine. Be that way!";
 
-    return is_yelling(phrase)
-               ? (is_question(phrase) ? "Calm down, I know what I'm doing!"
-                                      : "Whoa, chill out!")
-               : (is_question(phrase) ? "Sure." : "Whatever.");
+    if (is_yelling(phrase)) {
+        return (is_question(phrase) ? "Calm down, I know what I'm doing!"
+                                    : "Whoa, chill out!");
+    }
+
+    return is_question(phrase) ? "Sure." : "Whatever.";
 }
 } // namespace bob
