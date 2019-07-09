d6275739d317f1cdf7ee33b3b4bf278e0f40f0eb
Ivan Krechetov <ikr@ikr.su>
2019-07-09 15:08:56 +0200

    Optimize slightly

--- cpp/etl/etl.cpp
+++ cpp/etl/etl.cpp
@@ -6,7 +6,7 @@ std::map<char, int> transform(const std::map<int, std::vector<char>> &old) {
     static std::locale loc{};
     std::map<char, int> ans;
 
-    for (const auto kv : old) {
+    for (const auto &kv : old) {
         for (const auto &v : kv.second) {
             ans[std::tolower(v, loc)] = kv.first;
         }
