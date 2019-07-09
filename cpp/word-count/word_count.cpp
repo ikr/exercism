ee940d080cbe6d1f1380870d49fd20eb600dd9f5
Ivan Krechetov <ikr@ikr.su>
2019-07-09 15:27:24 +0200

    Bootstrap the word-count

---
+++ cpp/word-count/word_count.cpp
@@ -0,0 +1,7 @@
+#include "word_count.h"
+
+namespace word_count {
+std::map<std::string, int> words(const std::string &phrase) {
+    return std::map<std::string, int>{};
+}
+} // namespace word_count
