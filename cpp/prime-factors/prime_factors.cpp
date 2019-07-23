247c127612ad2e88503aff271564d31a40248bf8
Ivan Krechetov <ikr@ikr.su>
2019-07-23 08:43:42 +0200

    Reduce

--- cpp/prime-factors/prime_factors.cpp
+++ cpp/prime-factors/prime_factors.cpp
@@ -1,21 +1,17 @@
 #include "prime_factors.h"
 
 namespace prime_factors {
-std::vector<int> of(const int x) {
-    if (x < 2) return {};
-
+std::vector<int> of(int x) {
     std::vector<int> answer;
-    int remainder = x;
 
-    for (int divider = 2; divider * divider <= remainder; ++divider) {
-        while (remainder % divider == 0) {
+    for (int divider = 2; divider * divider <= x; ++divider) {
+        while (x % divider == 0) {
             answer.push_back(divider);
-            remainder /= divider;
+            x /= divider;
         }
     }
 
-    if (remainder != 1) answer.push_back(remainder);
-
+    if (x > 1) answer.push_back(x);
     return answer;
 }
 } // namespace prime_factors
