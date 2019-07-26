dcfedfcace15d28908de01f3eaf40fd8209f2c5e
Ivan Krechetov <ikr@ikr.su>
2019-07-26 19:23:57 +0200

    Bootstrap the pascals-triangle

---
+++ cpp/pascals-triangle/pascals_triangle.cpp
@@ -0,0 +1,13 @@
+#include "pascals_triangle.h"
+
+namespace {
+using Vec = std::vector<int>;
+using VVec = std::vector<Vec>;
+} // namespace
+
+namespace pascals_triangle {
+VVec generate_rows(unsigned int last_row_index) {
+    VVec ans(last_row_index);
+    return ans;
+}
+} // namespace pascals_triangle
---
+++ cpp/pascals-triangle/pascals_triangle.h
@@ -0,0 +1,10 @@
+#ifndef PASCALS_TRIANGLE_H
+#define PASCALS_TRIANGLE_H
+
+#include <vector>
+
+namespace pascals_triangle {
+std::vector<std::vector<int>> generate_rows(unsigned int last_row_index);
+}
+
+#endif // PASCALS_TRIANGLE_H
