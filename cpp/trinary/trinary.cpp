61d0c14a8af82a81fe3bc5477e65a8578b780a2a
Ivan Krechetov <ikr@ikr.su>
2019-07-26 16:38:54 +0200

    Bootstrap the “Trinary”

---
+++ cpp/trinary/trinary.cpp
@@ -0,0 +1,5 @@
+#include "trinary.h"
+
+namespace trinary {
+int to_decimal(const std::string &tri) { return tri.size(); }
+} // namespace trinary
---
+++ cpp/trinary/trinary.h
@@ -0,0 +1,10 @@
+#ifndef TRINARY_H
+#define TRINARY_H
+
+#include <string>
+
+namespace trinary {
+int to_decimal(const std::string &tri);
+}
+
+#endif // TRINARY_H
