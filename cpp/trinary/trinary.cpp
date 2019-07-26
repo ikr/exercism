7e87a6c25c1fef268b7fd0523669c44d4cb90959
Ivan Krechetov <ikr@ikr.su>
2019-07-26 16:46:16 +0200

    Implement Trinary

--- cpp/trinary/trinary.cpp
+++ cpp/trinary/trinary.cpp
@@ -1,5 +1,16 @@
 #include "trinary.h"
 
 namespace trinary {
-int to_decimal(const std::string &tri) { return tri.size(); }
+int to_decimal(const std::string &tri) {
+    int significance = 1;
+    int ans = 0;
+
+    for (auto it = tri.crbegin(); it != tri.crend(); ++it) {
+        if (*it < '0' || *it > '9') continue;
+        ans += (*it - '0') * significance;
+        significance *= 3;
+    }
+
+    return ans;
+}
 } // namespace trinary
