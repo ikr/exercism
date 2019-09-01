a6d8c17839dd335ee08a4e86decc6667b5c688a1
Ivan Krechetov <ikr@ikr.su>
2019-09-01 08:55:00 +0200

    Bootstrap the “Binary”

---
+++ cpp/binary/binary.cpp
@@ -0,0 +1,5 @@
+#include "binary.h"
+
+namespace binary {
+unsigned int convert(const std::string &bin_num) { return bin_num.size(); }
+} // namespace binary
---
+++ cpp/binary/binary.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_H
+#define BINARY_H
+
+#include <string>
+
+namespace binary {
+unsigned int convert(const std::string &bin_num);
+}
+
+#endif // BINARY_H
