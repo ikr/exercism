159c9259e21d6e4e7b91bbaffdd2a53a95c87eea
Ivan Krechetov <ikr@ikr.su>
2019-09-01 11:25:29 +0200

    Implement the “Binary”

--- cpp/binary/binary.cpp
+++ cpp/binary/binary.cpp
@@ -1,5 +1,19 @@
 #include "binary.h"
 
 namespace binary {
-unsigned int convert(const std::string &bin_num) { return bin_num.size(); }
+unsigned int convert(const std::string &bin_num) {
+    unsigned int multiplier = 1;
+    unsigned int answer = 0;
+
+    for (auto it = bin_num.crbegin(); it != bin_num.crend(); ++it) {
+        if (*it == '1')
+            answer += multiplier;
+        else if (*it != '0')
+            return 0;
+
+        multiplier *= 2;
+    }
+
+    return answer;
+}
 } // namespace binary
