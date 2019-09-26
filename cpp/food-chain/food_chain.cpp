f9d0aa39a6165d7a4ff1be499b1e7c2d4f3bcdd1
Ivan Krechetov <ikr@ikr.su>
2019-09-26 10:38:11 +0200

    Introduce the chain_definition

--- cpp/food-chain/food_chain.cpp
+++ cpp/food-chain/food_chain.cpp
@@ -1,11 +1,43 @@
 #include "food_chain.h"
 #include <cassert>
+#include <set>
+#include <vector>
+#include <sstream>
+
+namespace {
+enum class OptionalProperty { ADDENDUM, JUDGEMENT };
+
+struct Object {
+    std::string name;
+    std::set<OptionalProperty> present_optionals;
+    std::string addendum;
+    std::string judgement;
+};
+
+const std::vector<Object> chain_definition{
+    {"fly",
+     {OptionalProperty::JUDGEMENT},
+     "",
+     "I don't know why she swallowed the fly. Perhaps she'll die."}};
+} // namespace
 
 namespace food_chain {
 std::string verse(const unsigned int verse_num) {
-    assert(verse_num > 0U);
+    assert(0U < verse_num && verse_num <= chain_definition.size());
 
-    return "I know an old lady who swallowed a fly.\n"
-           "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+    const auto obj = chain_definition.at(verse_num - 1);
+
+    std::stringstream result;
+    result << "I know an old lady who swallowed a " << obj.name << ".\n";
+
+    if (obj.present_optionals.count(OptionalProperty::ADDENDUM)) {
+        result << "It " << obj.addendum << ".\n";
+    }
+
+    if (obj.present_optionals.count(OptionalProperty::JUDGEMENT)) {
+        result << obj.judgement << '\n';
+    }
+
+    return result.str();
 }
 } // namespace food_chain
