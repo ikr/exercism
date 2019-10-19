c6d50c28dc683c35ced731376f0c762c99cb4822
Ivan Krechetov <ikr@ikr.su>
2019-10-19 20:22:23 +0200

    Reduce symbols_by_value

--- cpp/roman-numerals/roman_numerals.cpp
+++ cpp/roman-numerals/roman_numerals.cpp
@@ -5,11 +5,9 @@
 
 namespace {
 static const std::map<int, std::string> symbols_by_value{
-    {1, "I"},    {4, "IV"},   {5, "V"},     {6, "VI"},    {7, "VII"},
-    {8, "VIII"}, {9, "IX"},   {10, "X"},    {40, "XL"},   {50, "L"},
-    {60, "LX"},  {70, "LXX"}, {80, "LXXX"}, {90, "XC"},   {100, "C"},
-    {400, "CD"}, {500, "D"},  {600, "DC"},  {700, "DCC"}, {800, "DCC"},
-    {900, "CM"}, {1000, "M"}};
+    {1, "I"},   {4, "IV"},   {5, "V"},   {9, "IX"},  {10, "X"},
+    {40, "XL"}, {50, "L"},   {90, "XC"}, {100, "C"}, {400, "CD"},
+    {500, "D"}, {900, "CM"}, {1000, "M"}};
 
 struct Numeral final {
     const int value;
