323a2c80d4aa1d5bdcf5ba91a035ae03e973f7c5
Ivan Krechetov <ikr@ikr.su>
2019-10-22 19:38:17 +0200

    Don't create an intermediate vector of int-s

--- cpp/series/series.cpp
+++ cpp/series/series.cpp
@@ -2,30 +2,25 @@
 #include <algorithm>
 #include <stdexcept>
 
-namespace {
-using vec = std::vector<int>;
-using vvec = std::vector<vec>;
-} // namespace
-
 namespace series {
-vec digits(const std::string &source) {
-    vec answer(source.size());
+std::vector<int> digits(const std::string &source) {
+    std::vector<int> answer(source.size());
     std::transform(source.cbegin(), source.cend(), answer.begin(),
                    [](const char d) { return d - '0'; });
     return answer;
 }
 
-vvec slice(const std::string &source, const int window_size) {
+std::vector<std::vector<int>> slice(const std::string &source,
+                                    const int window_size) {
     if (window_size <= 0 || window_size > static_cast<int>(source.size()))
         throw std::domain_error("Expected: 0 < window_size <= source_size");
 
-    const vec ds = digits(source);
-    vvec answer;
+    std::vector<std::vector<int>> answer;
 
-    for (auto first = ds.cbegin(), last = first + window_size;;
+    for (auto first = source.cbegin(), last = first + window_size;;
          ++first, ++last) {
-        answer.push_back(vec{first, last});
-        if (last == ds.cend()) break;
+        answer.push_back(digits(std::string(first, last)));
+        if (last == source.cend()) break;
     }
 
     return answer;
