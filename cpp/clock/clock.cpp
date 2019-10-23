cd1845aa5023d0050c4f1f90c4d631aada700969
Ivan Krechetov <ikr@ikr.su>
2019-10-23 17:20:14 +0200

    Apply formatting to the sstream

--- cpp/clock/clock.cpp
+++ cpp/clock/clock.cpp
@@ -1,9 +1,14 @@
 #include "clock.h"
+#include <iomanip>
+#include <sstream>
 
 namespace date_independent {
 namespace clock {
 std::string at(const int hour, const int minute) {
-    return std::to_string(hour) + ":" + std::to_string(minute);
+    std::stringstream ss;
+    ss << std::setfill('0') << std::setw(2) << (hour + minute / 60) % 24 << ':'
+       << std::setfill('0') << std::setw(2) << minute % 60;
+    return ss.str();
 }
 } // namespace clock
 } // namespace date_independent
