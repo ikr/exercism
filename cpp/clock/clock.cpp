03b5bf336aa5b1ec98a12bfe16ac3937e1984eff
Ivan Krechetov <ikr@ikr.su>
2019-10-23 19:11:42 +0200

    Hack on

--- cpp/clock/clock.cpp
+++ cpp/clock/clock.cpp
@@ -6,8 +6,8 @@ namespace date_independent {
 namespace clock {
 std::string at(const int hour, const int minute) {
     std::stringstream ss;
-    ss << std::setfill('0') << std::setw(2) << (hour + minute / 60) % 24 << ':'
-       << std::setfill('0') << std::setw(2) << minute % 60;
+    ss << std::setfill('0') << std::setw(2) << (240 + hour + minute / 60) % 24
+       << ':' << std::setfill('0') << std::setw(2) << ((6000 + minute) % 60);
     return ss.str();
 }
 } // namespace clock
