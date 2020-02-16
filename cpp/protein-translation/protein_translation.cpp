e4febb7acd0a0e7e424ded2b3747b51401445565
Ivan Krechetov <ikr@ikr.su>
2020-02-16 20:54:57 +0100

    Init the protein-translation

---
+++ cpp/protein-translation/protein_translation.cpp
@@ -0,0 +1,8 @@
+#include "protein_translation.h"
+
+namespace protein_translation {
+std::vector<std::string> proteins(const std::string &rna) {
+    assert(rna.size());
+    return {"Methionine"};
+}
+} // namespace protein_translation
