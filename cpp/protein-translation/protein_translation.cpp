974f11140a95da648c02b5cfe8a01cab4035e518
Ivan Krechetov <ikr@ikr.su>
2020-02-17 19:02:08 +0100

    Satisfy all the test cases

--- cpp/protein-translation/protein_translation.cpp
+++ cpp/protein-translation/protein_translation.cpp
@@ -9,7 +9,10 @@ std::map<std::string, std::string> map_codons_to_amino_acids() {
     std::map<std::string, std::string> ans;
 
     const std::vector<std::pair<std::vector<std::string>, std::string>> src{
-        {{"AUG"}, "Methionine"}};
+        {{"AUG"}, "Methionine"},      {{"UUU", "UUC"}, "Phenylalanine"},
+        {{"UUA", "UUG"}, "Leucine"},  {{"UCU", "UCC", "UCA", "UCG"}, "Serine"},
+        {{"UAU", "UAC"}, "Tyrosine"}, {{"UGU", "UGC"}, "Cysteine"},
+        {{"UGG"}, "Tryptophan"}};
 
     for (const auto &p : src) {
         for (const auto &codon : p.first) {
@@ -25,7 +28,17 @@ const auto amino_acids_by_codons = map_codons_to_amino_acids();
 
 namespace protein_translation {
 std::vector<std::string> proteins(const std::string &rna) {
-    assert(rna.size());
-    return {"Methionine"};
+    std::vector<std::string> ans;
+
+    for (std::size_t i = 0; i + 2 < rna.size(); i += 3) {
+        const auto codon = rna.substr(i, 3);
+
+        if (amino_acids_by_codons.count(codon))
+            ans.push_back(amino_acids_by_codons.at(codon));
+        else
+            break;
+    }
+
+    return ans;
 }
 } // namespace protein_translation
